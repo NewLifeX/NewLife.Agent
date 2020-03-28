54f87a78cbf0b7f09aedd6258a444f6811286f87
大石头 <nnhy@vip.qq.com>
2020-03-28 20:04:58 +0800

    NewLife.Agent 拆分，独立出去

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NewLife.Agent;
+
+namespace Test
+{
+    public class Program
+    {
+        private static void Main(String[] args)
+        {
+            Environment.SetEnvironmentVariable("DOTNET_SYSTEM_GLOBALIZATION_INVARIANT", "1");
+
+            var svc = new MyXService();
+            svc.Main();
+        }
+
+    }
+}
