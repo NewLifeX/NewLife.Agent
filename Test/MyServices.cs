7a0518c7bce6a0178dd04636a2a2140c94ed9e6b
大石头 <nnhy@vip.qq.com>
2020-07-03 14:44:22 +0800

    独立例子代码，避免误导新人

--- Test/MyServices.cs
+++ Test/MyServices.cs
@@ -1,34 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NewLife.Agent;
 using NewLife.Log;
 using NewLife.Threading;
 
 namespace Test
 {
-    public class MyServices:MyXService
+    /// <summary>代理服务例子。自定义服务程序可参照该类实现。</summary>
+    public class MyServices : ServiceBase
     {
+        #region 属性
+        #endregion
+
+        #region 构造函数
+        /// <summary>实例化一个代理服务</summary>
+        public MyServices()
+        {
+            // 一般在构造函数里面指定服务名
+            ServiceName = "XAgent";
+
+            DisplayName = "新生命服务代理";
+            Description = "用于承载各种服务的服务代理！";
+        }
+        #endregion
+
+        #region 核心
         private TimerX _timer;
+        private TimerX _timer2;
+        /// <summary>开始工作</summary>
+        /// <param name="reason"></param>
         protected override void StartWork(String reason)
         {
-            // 5秒开始，每10秒执行一次
-            _timer = new TimerX(DoWork, null, 5_000, 10_000) { Async = true };
+            WriteLog("业务开始……");
+
+            // 5秒开始，每60秒执行一次
+            _timer = new TimerX(DoWork, null, 5_000, 60_000) { Async = true };
+            // 每天凌晨执行一次
+            _timer2 = new TimerX(DoWork, null, DateTime.Today, 24 * 3600 * 1000) { Async = true };
+
+            base.StartWork(reason);
         }
 
-
-        /// <summary>
-        /// 实际负责的工作
-        /// </summary>
-        /// <param name="state"></param>
         private void DoWork(Object state)
         {
-            string data = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var data = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 
             //日志会输出到BinTest目录中
             XTrace.WriteLine($"代码执行时间：{data}");
         }
+
+        /// <summary>停止服务</summary>
+        /// <param name="reason"></param>
+        protected override void StopWork(String reason)
+        {
+            WriteLog("业务结束！{0}", reason);
+
+            _timer.Dispose();
+            _timer2.Dispose();
+
+            base.StopWork(reason);
+        }
+        #endregion
     }
 }
