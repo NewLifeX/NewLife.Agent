3e7e6c1c7221548703f96d71cb7fe531e5dfd4cf
大石头 <nnhy@vip.qq.com>
2023-07-26 15:47:31 +0800

    依赖网络

--- Zero.Web/Program.cs
+++ Zero.Web/Program.cs
@@ -4,64 +4,72 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using NewLife.Agent;
 
-namespace Zero.Web
+namespace Zero.Web;
+
+public class Program
 {
-    public class Program
+    private static void Main(String[] args) => new MyServices { Args = args }.Main(args);
+}
+
+/// <summary>代理服务例子。自定义服务程序可参照该类实现。</summary>
+public class MyServices : ServiceBase
+{
+    #region 属性
+    /// <summary>性能跟踪器</summary>
+    public String[] Args { get; set; }
+    #endregion
+
+    #region 构造函数
+    /// <summary>实例化一个代理服务</summary>
+    public MyServices()
     {
-        private static void Main(String[] args) => new MyServices { Args = args }.Main(args);
+        // 一般在构造函数里面指定服务名
+        ServiceName = "WebAgent";
+
+        DisplayName = "Web服务代理";
+        Description = "用于承载各种服务的服务代理！";
     }
 
-    /// <summary>代理服务例子。自定义服务程序可参照该类实现。</summary>
-    public class MyServices : ServiceBase
+    protected override void Init()
     {
-        #region 属性
-        /// <summary>性能跟踪器</summary>
-        public String[] Args { get; set; }
-        #endregion
+        base.Init();
 
-        #region 构造函数
-        /// <summary>实例化一个代理服务</summary>
-        public MyServices()
-        {
-            // 一般在构造函数里面指定服务名
-            ServiceName = "WebAgent";
-
-            DisplayName = "Web服务代理";
-            Description = "用于承载各种服务的服务代理！";
-        }
-        #endregion
-
-        #region 核心
-        private CancellationTokenSource _source;
-        /// <summary>开始工作</summary>
-        /// <param name="reason"></param>
-        protected override void StartWork(String reason)
-        {
-            WriteLog("业务开始……");
-
-            _source = new CancellationTokenSource();
-            CreateHostBuilder(Args).Build().RunAsync(_source.Token);
-
-            base.StartWork(reason);
-        }
-
-        public static IHostBuilder CreateHostBuilder(String[] args) =>
-            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
-        /// <summary>停止服务</summary>
-        /// <param name="reason"></param>
-        protected override void StopWork(String reason)
-        {
-            WriteLog("业务结束！{0}", reason);
-
-            _source.Cancel();
-
-            base.StopWork(reason);
-        }
-        #endregion
+        // 依赖网络
+        if (Host is Systemd sys)
+            sys.DependOnNetwork = true;
     }
+    #endregion
+
+    #region 核心
+    private CancellationTokenSource _source;
+    /// <summary>开始工作</summary>
+    /// <param name="reason"></param>
+    protected override void StartWork(String reason)
+    {
+        WriteLog("业务开始……");
+
+        _source = new CancellationTokenSource();
+        CreateHostBuilder(Args).Build().RunAsync(_source.Token);
+
+        base.StartWork(reason);
+    }
+
+    public static IHostBuilder CreateHostBuilder(String[] args) =>
+        Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+
+    /// <summary>停止服务</summary>
+    /// <param name="reason"></param>
+    protected override void StopWork(String reason)
+    {
+        WriteLog("业务结束！{0}", reason);
+
+        _source.Cancel();
+
+        base.StopWork(reason);
+    }
+    #endregion
 }
